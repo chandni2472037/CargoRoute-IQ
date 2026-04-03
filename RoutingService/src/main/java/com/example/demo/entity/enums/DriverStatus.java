a7d57e11c482c126f5959d66ae8ef42e574da8c8
Parsa <2471885@cognizant.com>
2026-04-03 14:21:16 +0530

    added fleet and routes

--- RoutingService/src/main/java/com/example/demo/entity/enums/DriverStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity.enums;
-
-public enum DriverStatus {
-	    AVAILABLE,          
-	    ASSIGNED,           
-	    ON_ROUTE,           
-	    UNAVAILABLE,        
-	    SUSPENDED           
-}
