9257f4cae06b57a63ec2f6bb762a7ad39296a69c
Mamatha <2471986@cognizant.com>
2026-04-03 11:54:09 +0530

    Removed KpiService from repo

--- KpiService/src/main/java/com/example/demo/Exception/KPINotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Exception;
-
-public class KPINotFoundException extends RuntimeException {
-    public KPINotFoundException(String message) {
-        super(message);
-    }
-}
