2712549f621a319340dc872e25e200c92370b83d
Mamatha <2471986@cognizant.com>
2026-04-03 11:55:44 +0530

    Removed ReportService from repo

--- ReportService/src/main/java/com/example/demo/enums/ReportScope.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.enums;
-
-public enum ReportScope {
-	UTILIZATION,
-    ONTIME,
-    REVENUE,
-    EXCEPTIONS
-
-}
--- ReportService/src/main/java/com/example/demo/repo/ReportRepo.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.demo.repo;
-import org.springframework.data.jpa.repository.JpaRepository; 
-
-import com.example.demo.entity.Report; 
- 
-public interface ReportRepo extends JpaRepository<Report, Long> { 
-} 
