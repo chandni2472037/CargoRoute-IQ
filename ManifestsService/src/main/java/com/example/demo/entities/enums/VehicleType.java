9be15c2c26600d46465b91619e8a31c68934715e
Siddam-Sai-Meghana <Siddam.SaiMeghana@cognizant.com>
2026-04-17 17:40:31 +0530

    Delete ManifestsService directory

--- ManifestsService/src/main/java/com/example/demo/entities/enums/VehicleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.entities.enums;
-
-public enum VehicleType {
-	 VAN, TRUCK, TRAILER
-
-}
--- ManifestsService/src/main/java/com/example/demo/exception/ProofOfDeliveryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ProofOfDeliveryNotFoundException extends RuntimeException {
-    public ProofOfDeliveryNotFoundException(String message) {
-        super(message);
-    }
-}
