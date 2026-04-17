ca6ba0f974854427da2f0e2d488a9d3f70149cd0
Siddam-Sai-Meghana <Siddam.SaiMeghana@cognizant.com>
2026-04-17 17:40:12 +0530

    Delete DispatchService directory

--- DispatchService/src/main/java/com/example/demo/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
